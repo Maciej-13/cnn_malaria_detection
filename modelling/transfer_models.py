ef075380f3945ab3c0277a0073b450cf45b5df02
Maciej <maciej.wysocki13@gmail.com>
2020-12-11 15:35:15 +0100

    Add class for ResNet50, InceptionResNetV2, NasNetMobile, MobileNetV2

--- modelling/transfer_models.py
+++ modelling/transfer_models.py
@@ -44,9 +44,6 @@ class VGG19(CNN, PreTrainedModel):
         model = keras.Sequential([
             self._base_model,
 
-            layers.Flatten(),
-
-            layers.Dense(units=128, activation=activation),
             layers.Dropout(dropout_rate),
 
             layers.Dense(units=1, activation='sigmoid')
@@ -56,5 +53,118 @@ class VGG19(CNN, PreTrainedModel):
     @beartype
     def __create_base_model(self, input_shape: tuple):
         base_model = keras.applications.VGG19(include_top=False, weights='imagenet', input_shape=input_shape)
+        base_model.trainable = False
+        return base_model
+
+
+class ResNet50(CNN, PreTrainedModel):
+
+    def __init__(self, input_shape: tuple, activation: str = 'relu', dropout_rate: float = 0.0):
+        super().__init__()
+        self._base_model = self.__create_base_model(input_shape)
+        self._model = self.__create_model(input_shape, activation, dropout_rate)
+
+    def fine_tuning(self, training_data, initial_epochs: int, fine_tune_epochs: int, **kwargs):
+        epochs = fine_tune_epochs + initial_epochs
+        return self._model.fit(training_data, epochs=epochs, initial_epoch=initial_epochs, **kwargs)
+
+    @beartype
+    def __create_model(self, input_shape: tuple, activation: str, dropout_rate: float):
+        inputs = keras.Input(shape=input_shape)
+        x = self._base_model(inputs, training=False)
+        x = layers.GlobalMaxPooling2D()(x)
+        x = keras.layers.Dropout(dropout_rate)(x)
+        outputs = layers.Dense(units=1, activation="sigmoid")(x)
+        model = keras.Model(inputs, outputs)
+        return model
+
+    @beartype
+    def __create_base_model(self, input_shape: tuple):
+        base_model = keras.applications.ResNet50(include_top=False, weights='imagenet', input_shape=input_shape)
+        base_model.trainable = False
+        return base_model
+
+
+class InceptionResNetV2(CNN, PreTrainedModel):
+
+    def __init__(self, input_shape: tuple, activation: str = 'relu', dropout_rate: float = 0.0):
+        super().__init__()
+        self._base_model = self.__create_base_model(input_shape)
+        self._model = self.__create_model(input_shape, activation, dropout_rate)
+
+    def fine_tuning(self, training_data, initial_epochs: int, fine_tune_epochs: int, **kwargs):
+        epochs = fine_tune_epochs + initial_epochs
+        return self._model.fit(training_data, epochs=epochs, initial_epoch=initial_epochs, **kwargs)
+
+    @beartype
+    def __create_model(self, input_shape: tuple, activation: str, dropout_rate: float):
+        inputs = keras.Input(shape=input_shape)
+        x = self._base_model(inputs, training=False)
+        x = layers.GlobalMaxPooling2D()(x)
+        x = keras.layers.Dropout(dropout_rate)(x)
+        outputs = layers.Dense(units=1, activation="sigmoid")(x)
+        model = keras.Model(inputs, outputs)
+        return model
+
+    @beartype
+    def __create_base_model(self, input_shape: tuple):
+        base_model = keras.applications.InceptionResNetV2(input_shape=input_shape, include_top=False,
+                                                          weights='imagenet')
+        base_model.trainable = False
+        return base_model
+
+
+class NasNetMobile(CNN, PreTrainedModel):
+
+    def __init__(self, input_shape: tuple, activation: str = 'relu', dropout_rate: float = 0.0):
+        super().__init__()
+        self._base_model = self.__create_base_model(input_shape)
+        self._model = self.__create_model(input_shape, activation, dropout_rate)
+
+    def fine_tuning(self, training_data, initial_epochs: int, fine_tune_epochs: int, **kwargs):
+        epochs = fine_tune_epochs + initial_epochs
+        return self._model.fit(training_data, epochs=epochs, initial_epoch=initial_epochs, **kwargs)
+
+    @beartype
+    def __create_model(self, input_shape: tuple, activation: str, dropout_rate: float):
+        inputs = keras.Input(shape=input_shape)
+        x = self._base_model(inputs, training=False)
+        x = layers.GlobalMaxPooling2D()(x)
+        x = keras.layers.Dropout(dropout_rate)(x)
+        outputs = layers.Dense(units=1, activation="sigmoid")(x)
+        model = keras.Model(inputs, outputs)
+        return model
+
+    @beartype
+    def __create_base_model(self, input_shape: tuple):
+        base_model = keras.applications.NASNetMobile(input_shape=input_shape, include_top=False, weights='imagenet')
+        base_model.trainable = False
+        return base_model
+
+
+class MobileNetV2(CNN, PreTrainedModel):
+
+    def __init__(self, input_shape: tuple, activation: str = 'relu', dropout_rate: float = 0.0):
+        super().__init__()
+        self._base_model = self.__create_base_model(input_shape)
+        self._model = self.__create_model(input_shape, activation, dropout_rate)
+
+    def fine_tuning(self, training_data, initial_epochs: int, fine_tune_epochs: int, **kwargs):
+        epochs = fine_tune_epochs + initial_epochs
+        return self._model.fit(training_data, epochs=epochs, initial_epoch=initial_epochs, **kwargs)
+
+    @beartype
+    def __create_model(self, input_shape: tuple, activation: str, dropout_rate: float):
+        inputs = keras.Input(shape=input_shape)
+        x = self._base_model(inputs, training=False)
+        x = layers.GlobalMaxPooling2D()(x)
+        x = keras.layers.Dropout(dropout_rate)(x)
+        outputs = layers.Dense(units=1, activation="sigmoid")(x)
+        model = keras.Model(inputs, outputs)
+        return model
+
+    @beartype
+    def __create_base_model(self, input_shape: tuple):
+        base_model = keras.applications.MobileNetV2(input_shape=input_shape, include_top=False, weights='imagenet')
         base_model.trainable = False
         return base_model
